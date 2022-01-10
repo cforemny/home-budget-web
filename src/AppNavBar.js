22b711dbe912e48948e99add71cf09e8b2f9bf8e
Cyprian Foremny <cforemny@gmail.com>
2022-01-10 21:07:35 +0100

    changed port

--- src/AppNavBar.js
+++ src/AppNavBar.js
@@ -1,61 +1,59 @@
 import React, {Component} from 'react';
 
-import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from 'reactstrap';
-
 export default class AppNavBar extends Component {
 
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.toggle = this.toggle.bind(this);
+    //     this.state = {
+    //         isOpen: false
+    //     };
+    // }
+    //
+    // toggle() {
+    //     this.setState({
+    //         isOpen: !this.state.isOpen
+    //     });
+    // }
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
-
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-
-    logout() {
-        sessionStorage.setItem('isUserValid', 'false');
-        window.location.reload(false);
-    }
+    // logout() {
+    //     sessionStorage.setItem('isUserValid', 'false');
+    //     window.location.reload(false);
+    // }
 
     render() {
         return (
             <div>
-                <Navbar color='light' light expand="md">
-                    <NavbarBrand href="/">Strona glowna</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle}/>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/expenses">Wydatki</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/incomes">Przychody</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/planner">Planer</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/summary">Podsumowanie</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/accounts">Stan kont</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/admin-panel">Panel administratora</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/" onClick={this.logout}>Wyloguj</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
+                {/*<Navbar color='light' light expand="md">*/}
+                {/*    <NavbarBrand href="/">Strona glowna</NavbarBrand>*/}
+                {/*    <NavbarToggler onClick={this.toggle}/>*/}
+                {/*    <Collapse isOpen={this.state.isOpen} navbar>*/}
+                {/*        <Nav className="ml-auto" navbar>*/}
+                {/*            <NavItem>*/}
+                {/*                <NavLink href="/expenses">Wydatki</NavLink>*/}
+                {/*            </NavItem>*/}
+                {/*            <NavItem>*/}
+                {/*                <NavLink href="/incomes">Przychody</NavLink>*/}
+                {/*            </NavItem>*/}
+                {/*            <NavItem>*/}
+                {/*                <NavLink href="/planner">Planer</NavLink>*/}
+                {/*            </NavItem>*/}
+                {/*            <NavItem>*/}
+                {/*                <NavLink href="/summary">Podsumowanie</NavLink>*/}
+                {/*            </NavItem>*/}
+                {/*            <NavItem>*/}
+                {/*                <NavLink href="/accounts">Stan kont</NavLink>*/}
+                {/*            </NavItem>*/}
+                {/*            <NavItem>*/}
+                {/*                <NavLink href="/admin-panel">Panel administratora</NavLink>*/}
+                {/*            </NavItem>*/}
+                {/*            <NavItem>*/}
+                {/*                <NavLink href="/" onClick={this.logout}>Wyloguj</NavLink>*/}
+                {/*            </NavItem>*/}
+                {/*        </Nav>*/}
+                {/*    </Collapse>*/}
+                {/*</Navbar>*/}
             </div>
         );
     }
